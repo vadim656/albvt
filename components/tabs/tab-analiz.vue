75ecd1fe1cd5b749313e7bc1fa05a597df706d5e
foxsis <va.four2016@gmail.com>
2022-10-07 17:48:14 +0300

    fix crossel

--- components/tabs/tab-analiz.vue
+++ components/tabs/tab-analiz.vue
@@ -2,6 +2,41 @@
   <div v-if="data" class='' v-show='isActive'>
     <p  class="text-[14px] pt-4">{{replaceDAta}}</p> 
     <!-- <p v-html="data" ></p> -->
+    <div class="my-4" v-if="crossel">
+      <span class="text-center w-full font-medium text-[16px]"
+                  >Состав комплекса:</span
+                >
+                <div class="flex flex-col gap-2 mt-4">
+                  <div
+                    v-for="(item, i) in crossel"
+                    :key="i"
+                    class="bg-white p-4 rounded-[5px] shadow-md  h-full grid grid-cols-5 items-center gap-4"
+                  >
+                    <div
+                      class="h-full flex flex-col gap-2 col-span-5 sm:col-span-4"
+                    >
+                      <nuxt-link
+                        :to="
+                          `/all-analyzes/in-complecs/` +
+                            item.categories[0].id +
+                            '/' +
+                            item.id
+                        "
+                        class="text-[14px] sm:text-[16px] text-[#4CBDEE]"
+                        >{{ item.name }}</nuxt-link
+                      >
+                      <span class="text-[14px] text-[#343434]/70">
+                        Код: {{ item.attributes[2].options[0] }}
+                      </span>
+                    </div>
+                    <div
+                      class="font-medium text-[14px] sm:text-[16px] h-full col-span-5 sm:col-span-1 flex justify-end"
+                    >
+                      {{ item.price }} ₽
+                    </div>
+                  </div>
+                </div>
+    </div>
   </div>
 </template>
 
@@ -17,6 +52,9 @@ export default {
     title: {
       type: String,
       default: 'Tab'
+    },
+    crossel: {
+      type: Array
     },
     data: {
       type: String,
--- pages/all-complecs/_complecs/_sht/_id.vue
+++ pages/all-complecs/_complecs/_sht/_id.vue
@@ -56,46 +56,15 @@
                 title="Описание"
                 :key="products.name + '1'"
                 :data="products.description"
+                :crossel="crossel"
                 class="cursor-pointer flex flex-col gap-4"
                 v-if="products.description.length"
               >
-                <div
+                <!-- <div
                   v-html="products.description"
                   class="text-[14px] pt-4"
-                ></div>
-                <span class="text-center w-full font-medium text-[16px]"
-                  >Состав комплекса:</span
-                >
-                <div class="flex flex-col gap-2">
-                  <div
-                    v-for="(item, i) in crossel"
-                    :key="i"
-                    class="bg-white p-4 rounded-[5px] shadow-md  h-full grid grid-cols-5 items-center gap-4"
-                  >
-                    <div
-                      class="h-full flex flex-col gap-2 col-span-5 sm:col-span-4"
-                    >
-                      <nuxt-link
-                        :to="
-                          `/all-analyzes/in-complecs/` +
-                            item.categories[0].id +
-                            '/' +
-                            item.id
-                        "
-                        class="text-[14px] sm:text-[16px] text-[#4CBDEE]"
-                        >{{ item.name }}</nuxt-link
-                      >
-                      <span class="text-[14px] text-[#343434]/70">
-                        Код: {{ item.attributes[2].options[0] }}
-                      </span>
-                    </div>
-                    <div
-                      class="font-medium text-[14px] sm:text-[16px] h-full col-span-5 sm:col-span-1 flex justify-end"
-                    >
-                      {{ item.price }} ₽
-                    </div>
-                  </div>
-                </div>
+                ></div> -->
+                
               </tab-analiz>
               <tab-analiz
                 :data="products.short_description"
